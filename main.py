d359c2a97c265c4a057abf0064ee0c0f03f07ecb
Starxueshu <108866584+Starxueshu@users.noreply.github.com>
2023-07-18 17:13:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import joblib as jl
 import pandas as pd
 import streamlit as st
 
-st.header("Development and validation of an interpretable model to predict intraoperative massive blood loss among metastatic spinal disease using machine learning techniques")
+st.header("Development and validation of a web-based AI prediction model to assess intraoperative massive blood loss among metastatic spinal disease using machine learning techniques")
 st.sidebar.title("Parameters Selection Panel")
 st.sidebar.markdown("Picking up parameters")
 Tumortype = st.sidebar.selectbox("Tumor type", ("Thyroid cancer", "Prostate cancer", "Breast cancer", "Renal cancer", "Lung cancer", "Hepatocellular carcinoma", "Gastrointestinal system cancer", "Urogenital cancer", "Others"))
@@ -32,8 +32,8 @@ if st.button("Submit"):
     else:
         st.text(f"Risk group: High-risk group")
     if prediction < 0.332:
-        st.markdown(f"Recommendation: Routine preoperative evaluation and management with a multidisciplinary approach.")
+        st.markdown(f"Regarding low-risk individuals, while those have a lower likelihood of experiencing massive blood loss, it is essential to closely monitor their intraoperative blood loss and coagulation parameters during surgery. This enables prompt detection and intervention in case of unexpected bleeding. Furthermore, in individuals assessed as low-risk, a more conservative approach to blood management may be suitable. This includes avoiding unnecessary blood product transfusions and restricting interventions to specific clinical indications.")
     else:
-        st.markdown(f"Recommendation: A multidisciplinary approach involving surgeons, anesthesiologists, and hematologists is crucial to ensure optimal outcomes for patients undergoing surgery for spinal metastases, particularly those in the high-risk group. Furthermore, the study recommends improving platelet counts before surgery, quitting smoking, and making appropriate surgical plans to decrease intraoperative bleeding. Intraoperative monitoring can also effectively manage bleeding complications.")
+        st.markdown(f"High-risk individuals should be thoroughly evaluated and optimized before surgery. This includes managing any underlying comorbidities, optimizing blood parameters (such as platelet count), and considering preoperative pharmacological interventions to improve coagulation function. In addition, a comprehensive blood management plan should be implemented for high-risk patients. This may involve preoperative autologous blood donation, intraoperative cell salvage, or targeted administration of blood products based on individual needs. What’s more, spine surgeons should consider employing strategies to minimize blood loss during the surgical procedure, such as meticulous hemostasis, minimizing tissue trauma, and utilizing advanced surgical techniques, including minimally invasive approaches when appropriate.")
 st.subheader('Model information')
-st.markdown('The model was developed using the XGBoosting machine algorithm, achieving an area under the curve (AUC) of 0.857 [95%CI: 0.827, 0.877]. The external validation of the model resulted in an AUC of 0.809 [95%CI: 0.778, 0.860]. This online calculator is designed to evaluate the risk of intraoperative massive blood loss, specifically among patients with metastatic spinal tumors undergoing decompressive surgery. It is accessible at no cost and intended solely for research purposes.')
+st.markdown('The AI prediction model was developed using the XGBoosting machine algorithm, achieving an area under the curve (AUC) of 0.857 [95%CI: 0.827, 0.877]. The external validation of the model resulted in an AUC of 0.809 [95%CI: 0.778, 0.860]. This online AI calculator is designed to evaluate the risk of intraoperative massive blood loss specifically among patients with metastatic spinal tumors undergoing decompressive surgery. It is accessible at no cost and intended solely for research purposes.')
